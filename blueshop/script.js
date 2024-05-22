d3c47421a03e3c08d8406b986f311eaecb88e9c7
ilixteam <ilixteam.business@gmail.com>
2024-05-22 19:47:45 +0330

    Add files via upload

---
+++ blueshop/script.js
@@ -0,0 +1,35 @@
+// alert("برای اطلاع از جدید ترین محصولات اعلان هارا فعال کنید!");
+let text1 = "hello world";
+const pnumber  = 3.14;
+let name = "علی",family = "سلیمی" , loc="ایلام", age = 21 ;
+const year="ساله";
+const from="از";
+console.log(text1);
+console.log(pnumber);
+console.log(5*pnumber);
+console.log(pnumber+3);
+console.log(name,family,age);
+console.log(name);
+console.log(family);
+console.log(age);
+console.log(year+age);
+console.log(name,family,age,year,from,loc);
+let numone= 10;
+let numtwo = 5;
+let much = 12e5;
+let textnum="5"
+const result= numtwo + numone;
+const result2=numone**numtwo;
+console.log(result)
+console.log(typeof result)
+console.log(numone**numtwo)
+console.log(much/result2)
+console.log(--numone)
+console.log(numone)
+console.log(++numone)
+console.log(++numone)
+console.log(numone*=numtwo)
+console.log(numone)
+console.log(numone)
+console.log(numtwo%numone)
+console.log(numone+textnum)
